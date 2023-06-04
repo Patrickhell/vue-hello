4460429b1b9890ea68d88c19930742f1d16d849c
Patrickhell <patrickhell19@yahoo.fr>
2023-06-04 02:11:31 +0200

    Esercizio con Vuejs

---
+++ js/my_script.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+createApp({
+   
+data() {
+ return {
+    message : "I'm verry happy to learn this new programming language !",
+   image : "img/barbara-ramos-graphic-designer.jpg",
+};
+}
+
+
+}).mount("#app")
